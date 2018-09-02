cf760c5f5a4b11c141cbedecb0f32cd0945024fa
Quality Depiction, LLC <michaelrhyndress@qualitydepiction.com>
2018-09-02 23:46:13 +0000

    Add new file

---
+++ src/escapeHtml.js
@@ -0,0 +1,10 @@
+ "use strict";
+
+export default function escapeHTML(unsafe) {
+	return unsafe
+	.replace(/&/g, "&amp;")
+	.replace(/</g, "&lt;")
+	.replace(/>/g, "&gt;")
+	.replace(/"/g, "&quot;")
+	.replace(/'/g, "&#039;");
+ }
